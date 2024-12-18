9f7fc86dabe5959679a3e21b40da1c31eea6bb29
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2024-12-18 05:35:58 +0200

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,34 @@
+import { Geist, Geist_Mono } from 'next/font/google';
+import '@/assets/styles/globals.css';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata = {
+  title: 'Booking App | Book a Room for meeting',
+  description: 'Discover the best meeting rooms',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Header />
+        <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
